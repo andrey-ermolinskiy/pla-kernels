ccad50b1eac63390d60edc02ef1570ed88d339fc
Andrey Ermolinskiy <99094082+andrey-ermolinskiy@users.noreply.github.com>
2022-02-13 14:51:45 -0600

    Delete common_defs.h

--- common_defs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-// Matrix and vector element index.
-typedef uint32_t idx_t;
-
-// Print an error message and terminate the process with a non-zero exit code.
-void fatal(const std::string &errmsg);
