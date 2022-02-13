2d526410cb72ab5e2d9893c57c2e026b28603122
Andrey Ermolinskiy <99094082+andrey-ermolinskiy@users.noreply.github.com>
2022-02-13 14:51:51 -0600

    Delete common_defs.cpp

--- common_defs.cpp
+++
@@ -1,9 +0,0 @@
-#include "common_defs.h"
-
-#include <iostream>
-#include <cstdlib>
-
-void fatal(const std::string &errmsg) {
-  std::cerr << "Fatal error: " << errmsg << '\n';
-  exit(EXIT_FAILURE);
-}
